ea9897a8b3c2aa2473eb44903ee45441b5e3368e
jessica <lingyu.cheng001@gmail.com>
2021-11-05 17:48:45 +0800

    Revert "version 2.1 to add bus stop info"
    This reverts commit 8657880fa70dbad6489184cd86be0bcfd935680b.

--- nginx/js/app/app_mqtt_client.js
+++ nginx/js/app/app_mqtt_client.js
@@ -1,7 +1,6 @@
 // Get the hostname, assume mqtt broker and web server are in the same host
-var base_url = window.location.host; //Jessica comment for test
-console.log("mqtt url: " + base_url);
-//var base_url = "10.8.0.7";
+//var base_url = window.location.host; //Jessica comment for test
+var base_url = "10.8.0.7";
 var pathArray = base_url.split(':');
 var base_host = pathArray[0];
 
@@ -563,33 +562,44 @@ function handlePatronUsageInfoData(msg) {
 }
 
 function handleCardInfoData(msg) {
-    cardtype = msg.card_info.card_type;
-    console.log(cardtype);
-    document.getElementById("cardtype").value = cardtype;
 
-    cardnumber = msg.card_info.card_number;
-    document.getElementById("cardnumber").value = cardnumber;
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log( page );
+    if(page === "login.html")
+    {
+        document.getElementById("login-correct-msg").visible = true;
+    }
+    else if(page === "index.html")
+    {
+        cardtype = msg.card_info.card_type;
+        console.log(cardtype);
+        document.getElementById("cardtype").value = cardtype;
 
-    bcvname = msg.device_Info.equipment_Id;
-    document.getElementById("bcvname").value = bcvname;
+        cardnumber = msg.card_info.card_number;
+        document.getElementById("cardnumber").value = cardnumber;
 
-    document.getElementById('carddisplay').style.visibility='visible';
+        bcvname = msg.device_Info.equipment_Id;
+        document.getElementById("bcvname").value = bcvname;
 
-    if(cardtype == "CEPAS")
-        document.getElementById("carddisplay").src = "/images/Cepas_New_Ride.png";
-    else if(cardtype == "BANKCARD")
-        document.getElementById("carddisplay").src = "/images/Bankcard_usage.png";
-    else if(cardtype == "CEPAS TOKEN")
-        document.getElementById("carddisplay").src = "/images/CTC_usage.png";
-    else
-        document.getElementById('carddisplay').style.visibility='hidden';
-    setTimeout(function() {
-        console.log("timeout");
-        document.getElementById("cardtype").value = "";
-        document.getElementById("bcvname").value = "";
-        document.getElementById("cardnumber").value = "";
-        document.getElementById('carddisplay').style.visibility='hidden';
-     }, 3000);
+        document.getElementById('carddisplay').style.visibility='visible';
+
+        if(cardtype == "CEPAS")
+            document.getElementById("carddisplay").src = "/images/Cepas_New_Ride.png";
+        else if(cardtype == "BANKCARD")
+            document.getElementById("carddisplay").src = "/images/Bankcard_usage.png";
+        else if(cardtype == "CEPAS TOKEN")
+            document.getElementById("carddisplay").src = "/images/CTC_usage.png";
+        else
+            document.getElementById('carddisplay').style.visibility='hidden';
+        setTimeout(function() {
+            console.log("timeout");
+            document.getElementById("cardtype").value = "";
+            document.getElementById("bcvname").value = "";
+            document.getElementById("cardnumber").value = "";
+            document.getElementById('carddisplay').style.visibility='hidden';
+         }, 3000);
+    }
 }
 
 
